8ec3bd1d3648131ecad0b676a8396d3fcdd51032
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-03-27 21:03:07 +0300

    Learned 'createSlice' and 'useSelector' from redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./app/layout/Header";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import Counter from "./app/components/Counter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Header />
-  </React.StrictMode>
+  <div className="text-white">
+    <Provider store={store}>
+      <Header />
+      <Counter/>
+    </Provider>
+  </div>
 );
