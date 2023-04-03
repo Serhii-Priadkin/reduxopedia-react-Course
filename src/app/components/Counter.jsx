42c1f213c546f013fc0d5a970b15529123a74517
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-04-03 19:12:00 +0300

    Removed main exports to store.js

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -5,22 +5,21 @@ import {
   decrement,
   incrementMultiplier,
   decrementMultiplier,
-} from "../../redux/slice/counterSlice";
+} from "../../redux/store";
 import { useState } from "react";
 
 function Counter() {
   const count = useSelector((state) => state.counterStore.count);
   const dispatch = useDispatch();
   const [multiplier, setMultiplier] = useState(10);
-
   return (
     <div
-      className="mt-2 pt-3 pl-2 text-center"
+      className=" mt-2 pt-3 pl-2 text-center"
       style={{ borderTop: "1px solid #999" }}
     >
       <div className="text-white pb-2 h4">Counter : {count}</div>
       <div className="row">
-        <div className="p-4 col-12 col-md-6">
+        <div className=" p-4 col-12 col-md-6">
           <div className="border p-4">
             <h4 className="text-success pb-2">Basic Counter</h4>
             <button
@@ -38,14 +37,14 @@ function Counter() {
             </button>
           </div>
         </div>
-        <div className="p-4 col-12 col-md-6">
+        <div className=" p-4 col-12 col-md-6">
           <div className="border p-4">
             <h4 className="text-success pb-2">Multiplier Counter</h4>
             <div className="row">
               <div className="col-4 p-1">
                 <input
-                  type="text"
-                  placeholder="multiplier..."
+                  type="test"
+                  placeholder="multipler..."
                   className="form-control"
                   value={multiplier}
                   onChange={(e) => setMultiplier(e.target.value)}
--- src/app/components/DestinationList.jsx
+++ src/app/components/DestinationList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { destinationClicked } from "../../redux/slice/destinationSlice";
+import { destinationClicked } from "../../redux/store";
 
 function DestinationList() {
   const destinationList = useSelector(
